0234541426e9f4f727e10bc0b4b31ce8610faa83
Bilge <lagala999@gmail.com>
2020-07-07 12:56:06 +0300

    Updates

---
+++ precache-manifest.d745bfecd658b0895cc5c4b5c6c60627.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b5a9b649622e800c0c92c3515e933e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1c1907483e15c661821f",
+    "url": "/portfolio/static/css/main.1361cd3a.chunk.css"
+  },
+  {
+    "revision": "175fb821e2ed3e6327a7",
+    "url": "/portfolio/static/js/2.1efe940f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.1efe940f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c1907483e15c661821f",
+    "url": "/portfolio/static/js/main.765d55c1.chunk.js"
+  },
+  {
+    "revision": "b722e835aa173b05874f",
+    "url": "/portfolio/static/js/runtime-main.0bff7b88.js"
+  },
+  {
+    "revision": "dcc99bc2a2d4c111b3b771d43e8a8178",
+    "url": "/portfolio/static/media/bg.dcc99bc2.jpg"
+  },
+  {
+    "revision": "e1de1489c485491148df78d3c054fee5",
+    "url": "/portfolio/static/media/to-doer.e1de1489.png"
+  }
+]);
