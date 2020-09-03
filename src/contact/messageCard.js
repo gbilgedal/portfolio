705f53158aba1f1963bc20196091820e8902a4b9
Bilge <lagala999@gmail.com>
2020-09-03 15:39:43 +0300

    add functionality to messaging

--- src/contact/messageCard.js
+++ src/contact/messageCard.js
@@ -2,26 +2,79 @@ import React, { useState } from "react";
 import "./index.css";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-
+import emailjs from "emailjs-com";
 const MessageCard = () => {
   const [value, setValue] = React.useState("Controlled");
+  const [email, setEmail] = React.useState("");
+  const [name, setName] = React.useState("");
+  const [msg, setMsg] = React.useState("");
 
-  const handleChange = (event) => {
-    setValue(event.target.value);
+  let templateParams = {
+    from_name: name + " email:" + email,
+    to_name: "bilge",
+    subject: email,
+    message: msg,
   };
+
+  const handleChange = (param, e) => {
+    switch (param) {
+      case "msg":
+        setMsg(e.target.value);
+        break;
+      case "name":
+        setName(e.target.value);
+        break;
+      case "email":
+        setEmail(e.target.value);
+        break;
+      default:
+        break;
+    }
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .send(
+        "porfolio_messages",
+        "template_3posh5e",
+        templateParams,
+        "user_suDWm3odcFybLLUUwZ3OR"
+      )
+      .then((result) => {
+        document.forms["socialForm"].reset();
+      });
+  };
+
   return (
     <React.Fragment>
-      <form className="socialform" noValidate autoComplete="off">
-        <TextField id="standard-basic" label="Name" />
-        <TextField id="standard-basic" label="Email" />
+      <form
+        className="socialform"
+        id="socialForm"
+        noValidate
+        autoComplete="off"
+        onSubmit={handleSubmit}
+      >
+        <TextField
+          id="standard-basic"
+          label="Name"
+          onChange={(e) => handleChange("name", e)}
+        />
+        <TextField
+          id="standard-basic"
+          label="Email"
+          onChange={(e) => handleChange("email", e)}
+        />
         <TextField
           id="standard-multiline-static"
           label="Message"
           multiline
           rows={4}
           placeholder="Enter Your Message Here"
+          onChange={(e) => handleChange("msg", e)}
         />
-        <Button variant="contained">Send Message</Button>
+        <Button type="submit" variant="contained">
+          Send Message
+        </Button>
       </form>
     </React.Fragment>
   );
